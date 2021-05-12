ded1c67aac54c83ddb1892adf1f42a8b57c93e5e
Ozzie <mikeosmonson@gmail.com>
2021-05-12 16:01:00 -0600

    Add independent challenge update

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,10 +1,17 @@
 class Classroom
-  attr_
+  attr_reader :subject, :capacity, :students
 
-
-  def initialize('subject', 'capacity', 'students')
+  def initialize(subject, capacity)
     @subject = subject
     @capacity = capacity
     @students = []
+  end
+
+  def add_student(students)
+    @students << students
+  end
+
+  def yell_at_students
+    @students << students.upcase
   end
 end
